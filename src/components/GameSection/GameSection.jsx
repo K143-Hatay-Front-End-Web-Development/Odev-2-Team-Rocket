49666da8d982204b17c46fa24b3efa74655a3f6e
Melin Kan <melin.kan@outlook.com>
2022-08-09 18:22:41 +0300

    Implement game win logic

--- src/components/GameSection/GameSection.jsx
+++ src/components/GameSection/GameSection.jsx
@@ -8,53 +8,50 @@ import Button from "../Button/Button";
 import "./GameSection.css";
 
 // Game Winning Conditions
-const winConditions = {
-  row: [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-  ],
-  column: [
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-  ],
-  diagonal: [
-    [0, 4, 8],
-    [2, 4, 6],
-  ],
-};
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function GameSection() {
   // Game Board Status
   const [board, setBoard] = useState(Array(9).fill(""));
+  //Winner status
+  const [winner, setWinner] = useState(null);
 
   // Check Winner
   useEffect(() => {
     const checkWinner = (conditions) => {
-      return conditions;
+      for (let i = 0; i < conditions.length; i++) {
+        const [a, b, c] = conditions[i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+          return { letter: board[a], indexes: conditions[i] };
+        }
+      }
+      return null;
     };
 
-    checkWinner(winConditions);
+    setWinner(checkWinner(winConditions));
   }, [board]);
 
   return (
     <div className="game-wrapper">
-      <div className="btn-row">
-        <Button num={0} board={board} setBoard={setBoard} />
-        <Button num={1} board={board} setBoard={setBoard} />
-        <Button num={2} board={board} setBoard={setBoard} />
-      </div>
-      <div className="btn-row">
-        <Button num={3} board={board} setBoard={setBoard} />
-        <Button num={4} board={board} setBoard={setBoard} />
-        <Button num={5} board={board} setBoard={setBoard} />
-      </div>
-      <div className="btn-row">
-        <Button num={6} board={board} setBoard={setBoard} />
-        <Button num={7} board={board} setBoard={setBoard} />
-        <Button num={8} board={board} setBoard={setBoard} />
-      </div>
+      {board.map((_, index) => {
+        return (
+          <Button
+            winner={winner}
+            num={index}
+            board={board}
+            setBoard={setBoard}
+          />
+        );
+      })}
     </div>
   );
 }
